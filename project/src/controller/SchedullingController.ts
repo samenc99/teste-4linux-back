e1dd4b9647107cdb33cd32e7f8badfa65fe0795a
samenc99 <mateus.enc@gmail.com>
2021-08-12 16:16:20 -0300

    SchedullingController

---
+++ project/src/controller/SchedullingController.ts
@@ -0,0 +1,25 @@
+import {SchedulingBusiness} from "../business/SchedulingBusiness";
+import {Request, Response} from "express";
+
+export class SchedullingController{
+  public schedullingBusiness = new SchedulingBusiness()
+
+  toSchedule = async (req: Request, res: Response): Promise<void> => {
+    try {
+      await this.schedullingBusiness.toSchedule(req.body)
+      res.status(201).send()
+    } catch (err) {
+      res.status(err.statusCode).send({message: err.message})
+    }
+  }
+
+  getSchedule = async (req: Request, res: Response): Promise<any> => {
+    try {
+      const schedules = await this.schedullingBusiness.getSchedule(req.query)
+      res.status(200).send({schedules})
+    } catch (err) {
+      res.status(err.statusCode).send({message: err.message})
+    }
+  }
+
+}
